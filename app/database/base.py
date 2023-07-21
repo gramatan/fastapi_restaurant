12f689f2b4d04ab4331a85746cf53c3c38fbb9f2
gramatan <78417563+gramatan@users.noreply.github.com>
2023-07-21 15:22:25 +0600

    Fix issue with incorrect path to DB inside containers. add external access to the DB from outside of container

--- app/database/base.py
+++ app/database/base.py
@@ -2,11 +2,9 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-SQLALCHEMY_DATABASE_URL = "postgresql://ylab:no_secure_password@localhost/resto"
+SQLALCHEMY_DATABASE_URL = "postgresql://ylab:no_secure_password@db/resto"
 
-engine = create_engine(
-    SQLALCHEMY_DATABASE_URL
-)
+engine = create_engine(SQLALCHEMY_DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 Base = declarative_base()
