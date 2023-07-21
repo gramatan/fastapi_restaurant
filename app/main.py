63d0605f8f7ff3e5df34cc1340f4acfba6329860
gramatan <78417563+gramatan@users.noreply.github.com>
2023-07-21 21:31:07 +0600

    Handlers for menu requests

--- app/main.py
+++ app/main.py
@@ -1,10 +1,20 @@
-from fastapi import FastAPI
-from app.database.base import engine, SessionLocal, Base
-from app.models import Menu, SubMenu, Dish
+import logging
+
+from typing import List
+
+from fastapi import FastAPI, Depends, HTTPException
+from sqlalchemy import select, delete
+from sqlalchemy.orm import Session
+
+from app.database.base import SessionLocal, Menu
+from app.schemas.menu import MenuCreate, MenuUpdate, S_Menu
+
+from app.crud import create_menu, read_menu, update_menu, delete_menu, read_menus
+
+logging.basicConfig(level=logging.INFO)
 
 app = FastAPI()
 
-Base.metadata.create_all(bind=engine)
 
 def get_db():
     db = SessionLocal()
@@ -12,3 +22,46 @@ def get_db():
         yield db
     finally:
         db.close()
+
+
+@app.get("/api/v1/menus")
+def get_menus(db: Session = Depends(get_db)) -> List[S_Menu]:
+    menus = read_menus(db)
+    return menus
+
+
+@app.post("/api/v1/menus", status_code=201)
+def post_menu(menu: MenuCreate, db: Session = Depends(get_db)) -> S_Menu:
+    db_menu = create_menu(db, menu)
+    return db_menu
+
+@app.get("/api/v1/menus/{menu_id}")
+def get_menu(menu_id: int | str, db: Session = Depends(get_db)) -> S_Menu:
+    menu = read_menu(db, menu_id)
+    if menu is None:
+        raise HTTPException(status_code=404, detail="menu not found")
+    menu.id = str(menu.id)
+    return menu
+
+@app.patch("/api/v1/menus/{menu_id}")
+def patch_menu(menu_id: int, menu: MenuUpdate, db: Session = Depends(get_db)) -> S_Menu:
+    db_menu = db.get(Menu, menu_id)
+    if db_menu is None:
+        raise HTTPException(status_code=404, detail="Menu not found")
+    for var, value in vars(menu).items():
+        setattr(db_menu, var, value) if value else None
+    db.commit()
+    return db_menu
+
+
+@app.delete("/api/v1/menus/{menu_id}")
+def delete_menu(menu_id: int, db: Session = Depends(get_db)):
+    db_menu = db.get(Menu, menu_id)
+    if db_menu is None:
+        raise HTTPException(status_code=404, detail="Menu not found")
+    db.execute(delete(Menu).where(Menu.id == menu_id))
+    db.commit()
+    return {"message": f"Menu {menu_id} deleted successfully."}
+
+
+
