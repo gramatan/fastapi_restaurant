a1eaa64528544cf3c891149c0c298dc4abda22e1
gramatan <78417563+gramatan@users.noreply.github.com>
2023-07-27 14:42:20 +0600

    All handlers changed to work with async

--- app/main.py
+++ app/main.py
@@ -5,10 +5,8 @@ from typing import List
 from fastapi import FastAPI, Depends
 from sqlalchemy.orm import Session
 
-from app.database import SessionLocal
-from app.schemas import DishBase, DishResponse
-from app.schemas import MenuBase, MenuResponse
-from app.schemas import SubMenuResponse, SubMenuBase
+from app.database import async_session
+from app.schemas import DishBase, DishResponse, MenuBase, MenuResponse, SubMenuResponse, SubMenuBase
 
 from app.crud import del_dish, update_dish, read_dish, create_dish, read_dishes
 from app.crud import read_submenus, create_submenu, read_submenu, update_submenu, del_submenu
@@ -20,93 +18,99 @@ logging.basicConfig(level=logging.INFO)
 app = FastAPI()
 
 
-def get_db():
-    db = SessionLocal()
-    try:
+async def get_db():
+    async with async_session() as db:
         yield db
-    finally:
-        db.close()
 
 
 # All about our Menus:
 @app.get("/api/v1/menus")
-def get_menus(db: Session = Depends(get_db)) -> List[MenuResponse]:
-    menus = read_menus(db)
-    return menus
+async def get_menus(db: Session = Depends(get_db)) -> List[MenuResponse]:
+    response = await read_menus(db)
+    return response
 
 
 @app.post("/api/v1/menus", status_code=201)
-def post_menu(menu: MenuBase, db: Session = Depends(get_db)) -> MenuResponse:
-    db_menu = create_menu(db, menu)
-    return db_menu
+async def post_menu(menu: MenuBase, db: Session = Depends(get_db)) -> MenuResponse:
+    response = await create_menu(db, menu)
+    return response
 
 
 @app.get("/api/v1/menus/{menu_id}")
-def get_menu(menu_id: int | str, db: Session = Depends(get_db)) -> MenuResponse:
-    return read_menu(db, menu_id)
+async def get_menu(menu_id: int | str, db: Session = Depends(get_db)) -> MenuResponse:
+    response = await read_menu(db, menu_id)
+    return response
 
 
 @app.patch("/api/v1/menus/{menu_id}")
-def patch_menu(menu_id: int, menu: MenuBase, db: Session = Depends(get_db)) -> MenuResponse:
-    return update_menu(db, menu_id, menu)
+async def patch_menu(menu_id: int, menu: MenuBase, db: Session = Depends(get_db)) -> MenuResponse:
+    response = await update_menu(db, menu_id, menu)
+    return response
 
 
 @app.delete("/api/v1/menus/{menu_id}")
-def delete_menu(menu_id: int, db: Session = Depends(get_db)):
-    return del_menu(db, menu_id)
+async def delete_menu(menu_id: int, db: Session = Depends(get_db)):
+    response = await del_menu(db, menu_id)
+    return response
 
 
 # All about our Submenus.
 @app.get("/api/v1/menus/{menu_id}/submenus")
-def get_submenus(menu_id: int, db: Session = Depends(get_db)) -> List[SubMenuResponse]:
-    submenus = read_submenus(db, menu_id)
+async def get_submenus(menu_id: int, db: Session = Depends(get_db)) -> List[SubMenuResponse]:
+    submenus = await read_submenus(db, menu_id)
     return submenus
 
 
 @app.post("/api/v1/menus/{menu_id}/submenus", status_code=201)
-def post_submenu(submenu: SubMenuBase, menu_id: int, db: Session = Depends(get_db)) -> SubMenuResponse:
-    db_submenu = create_submenu(db, submenu, menu_id)
+async def post_submenu(submenu: SubMenuBase, menu_id: int, db: Session = Depends(get_db)) -> SubMenuResponse:
+    db_submenu = await create_submenu(db, submenu, menu_id)
     return db_submenu
 
 
 @app.get("/api/v1/menus/{menu_id}/submenus/{submenu_id}")
-def get_submenu(menu_id: int, submenu_id: int, db: Session = Depends(get_db)) -> SubMenuResponse:
-    return read_submenu(db, submenu_id)
+async def get_submenu(menu_id: int, submenu_id: int, db: Session = Depends(get_db)) -> SubMenuResponse:
+    response = await read_submenu(db, submenu_id)
+    return response
 
 
 @app.patch("/api/v1/menus/{menu_id}/submenus/{submenu_id}")
-def patch_submenu(submenu_id: int, submenu: SubMenuBase, menu_id: int, db: Session = Depends(get_db)) -> SubMenuResponse:
-    return update_submenu(db, submenu_id, submenu)
+async def patch_submenu(submenu_id: int, submenu: SubMenuBase, menu_id: int, db: Session = Depends(get_db)) -> SubMenuResponse:
+    response = await update_submenu(db, submenu_id, submenu)
+    return response
 
 
 @app.delete("/api/v1/menus/{menu_id}/submenus/{submenu_id}")
-def delete_submenu(menu_id: int, submenu_id: int, db: Session = Depends(get_db)):
-    return del_submenu(db, submenu_id)
+async def delete_submenu(menu_id: int, submenu_id: int, db: Session = Depends(get_db)):
+    response = await del_submenu(db, submenu_id)
+    return response
 
 
 # All about our Dishes:
 @app.get("/api/v1/menus/{menu_id}/submenus/{submenu_id}/dishes")
-def get_dishes(db: Session = Depends(get_db)):
-    dishes = read_dishes(db)
+async def get_dishes(db: Session = Depends(get_db)):
+    dishes = await read_dishes(db)
     return dishes
 
 
 @app.post("/api/v1/menus/{menu_id}/submenus/{submenu_id}/dishes", status_code=201)
-def post_dish(dish: DishBase, submenu_id: int, db: Session = Depends(get_db)) -> DishResponse:
-    db_dish = create_dish(db, dish, submenu_id)
+async def post_dish(dish: DishBase, submenu_id: int, db: Session = Depends(get_db)) -> DishResponse:
+    db_dish = await create_dish(db, dish, submenu_id)
     return db_dish
 
 
 @app.get("/api/v1/menus/{menu_id}/submenus/{submenu_id}/dishes/{dish_id}")
-def get_dish(dish_id: int | str, db: Session = Depends(get_db)) -> DishResponse:
-    return read_dish(db, dish_id)
+async def get_dish(dish_id: int | str, db: Session = Depends(get_db)) -> DishResponse:
+    response = await read_dish(db, dish_id)
+    return response
 
 
 @app.patch("/api/v1/menus/{menu_id}/submenus/{submenu_id}/dishes/{dish_id}")
-def patch_dish(dish_id: int, dish: DishBase, db: Session = Depends(get_db)) -> DishResponse:
-    return update_dish(db, dish_id, dish)
+async def patch_dish(dish_id: int, dish: DishBase, db: Session = Depends(get_db)) -> DishResponse:
+    response = await update_dish(db, dish_id, dish)
+    return response
 
 
 @app.delete("/api/v1/menus/{menu_id}/submenus/{submenu_id}/dishes/{dish_id}")
-def delete_dish(dish_id: int, db: Session = Depends(get_db)) -> dict:
-    return del_dish(db, dish_id)
+async def delete_dish(dish_id: int, db: Session = Depends(get_db)) -> dict:
+    response = await del_dish(db, dish_id)
+    return response
