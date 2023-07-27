4ac023104a73cf3293160da25b42c05a16a9e414
gramatan <78417563+gramatan@users.noreply.github.com>
2023-07-27 14:46:56 +0600

    CRUD functions for menu update for work with async

--- app/crud/menu.py
+++ app/crud/menu.py
@@ -1,13 +1,14 @@
 from fastapi import HTTPException
-from sqlalchemy import delete
+from sqlalchemy import delete, select
 from sqlalchemy.orm import Session
 from app.database import Menu
 from app.schemas import MenuResponse, MenuBase
 import logging
 
 
-def read_menus(db: Session) -> list[MenuResponse]:
-    menus = db.query(Menu).all()
+async def read_menus(db: Session) -> list[MenuResponse]:
+    db_request = await db.execute(select(Menu))
+    menus = db_request.scalars().all()
     menus_list = []
     for menu in menus:
         menu_response = MenuResponse(**menu.__dict__)
@@ -16,46 +17,46 @@ def read_menus(db: Session) -> list[MenuResponse]:
     return menus_list
 
 
-def create_menu(db: Session, menu: MenuBase) -> MenuResponse:
+async def create_menu(db: Session, menu: MenuBase) -> MenuResponse:
     logging.info(menu)
     db_menu = Menu(**menu.model_dump())
     db.add(db_menu)
-    db.commit()
-    db.refresh(db_menu)
+    await db.commit()
+    await db.refresh(db_menu)
     db_menu_dict = db_menu.__dict__
     db_menu_dict["id"] = str(db_menu_dict["id"])
 
     return MenuResponse(**db_menu_dict)
 
 
-def read_menu(db: Session, menu_id: int) -> MenuResponse:
-    db_menu = db.query(Menu).filter(Menu.id == menu_id).first()
+async def read_menu(db: Session, menu_id: int) -> MenuResponse:
+    db_menu = await db.get(Menu, int(menu_id))
     if db_menu is None:
         raise HTTPException(status_code=404, detail="menu not found")
-    db.refresh(db_menu)
+    await db.refresh(db_menu)
     menu_dict = db_menu.__dict__
     menu_dict["id"] = str(menu_dict["id"])
     return MenuResponse(**menu_dict)
 
 
-def update_menu(db: Session, menu_id: int, menu: MenuBase) -> MenuResponse:
-    db_menu = db.get(Menu, menu_id)
+async def update_menu(db: Session, menu_id: int, menu: MenuBase) -> MenuResponse:
+    db_menu = await db.get(Menu, menu_id)
     if db_menu is None:
         raise HTTPException(status_code=404, detail="menu not found")
     for var, value in vars(menu).items():
         setattr(db_menu, var, value) if value else None
-    db.commit()
-    db.refresh(db_menu)
+    await db.commit()
+    await db.refresh(db_menu)
     db_menu_dict = db_menu.__dict__
     db_menu_dict["id"] = str(db_menu_dict["id"])
 
     return MenuResponse(**db_menu_dict)
 
 
-def del_menu(db: Session, menu_id: int) -> dict:
-    db_menu = db.get(Menu, menu_id)
+async def del_menu(db: Session, menu_id: int) -> dict:
+    db_menu = await db.get(Menu, menu_id)
     if db_menu is None:
         raise HTTPException(status_code=404, detail="menu not found")
-    db.execute(delete(Menu).where(Menu.id == menu_id))
-    db.commit()
+    await db.execute(delete(Menu).where(Menu.id == menu_id))
+    await db.commit()
     return {"message": f"Menu {menu_id} deleted successfully."}
