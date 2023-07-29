3e5239266f99319ecbd9ac0462f23a2b9b2bf50b
gramatan <78417563+gramatan@users.noreply.github.com>
2023-07-29 11:08:12 +0600

    Cleanup after tests added

--- app/tests/test_menu.py
+++ app/tests/test_menu.py
@@ -1,11 +1,10 @@
 import pytest
 from fastapi import HTTPException
+from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession
 
-from app.database import Menu
 from app.crud import menu as crud_menu
 from app.schemas import MenuBase
 
-from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession
 from app.database.base import SQLALCHEMY_DATABASE_URL
 
 
@@ -22,20 +21,20 @@ async def test_create_and_read_menu(db_session: AsyncSession) -> None:
     async for session in db_session:
         # Create
         menu_in = MenuBase(title="Test menu", description="Test menu description")
+        menu = await crud_menu.create_menu(session, menu_in)
+        menu_id = menu.id
 
-        db_menu = Menu(**menu_in.model_dump())
-        session.add(db_menu)
-        await session.commit()
-        await session.refresh(db_menu)
-
-        assert db_menu.title == menu_in.title
-        assert db_menu.description == menu_in.description
+        assert menu.title == menu_in.title
+        assert menu.description == menu_in.description
 
         # Read
-        menu_from_db = await crud_menu.read_menu(session, db_menu.id)
+        menu_from_db = await crud_menu.read_menu(session, menu_id)
 
         assert menu_from_db.title == menu_in.title
         assert menu_from_db.description == menu_in.description
+
+        # cleanup
+        await crud_menu.del_menu(session, menu_id)
 
 
 @pytest.mark.asyncio
@@ -44,14 +43,17 @@ async def test_update_menu(db_session: AsyncSession) -> None:
         # Create
         menu_in = MenuBase(title="Test menu", description="Test menu description")
         menu = await crud_menu.create_menu(session, menu_in)
+        menu_id = menu.id
 
         # Update
         menu_update = MenuBase(title="New name", description="New description")
-        updated_menu = await crud_menu.update_menu(session, menu.id, menu_update)
+        updated_menu = await crud_menu.update_menu(session, menu_id, menu_update)
 
         assert updated_menu.title == menu_update.title
         assert updated_menu.description == menu_update.description
 
+        # cleanup
+        await crud_menu.del_menu(session, menu_id)
 
 @pytest.mark.asyncio
 async def test_delete_menu(db_session: AsyncSession) -> None:
@@ -60,13 +62,13 @@ async def test_delete_menu(db_session: AsyncSession) -> None:
         menu_in = MenuBase(title="Test menu", description="Test menu description")
 
         menu = await crud_menu.create_menu(session, menu_in)
+        menu_id = menu.id
 
         # Delete
-        delete_response = await crud_menu.del_menu(session, menu.id)
-
-        # Deletion was successful
-        assert delete_response == {"message": f"Menu {menu.id} deleted successfully."}
+        delete_response = await crud_menu.del_menu(session, menu_id)
+        assert delete_response == {"message": f"Menu {menu_id} deleted successfully."}
 
         # Expect an HTTPException
         with pytest.raises(HTTPException):
-            await crud_menu.read_menu(session, menu.id)
+            await crud_menu.read_menu(session, menu_id)
+
