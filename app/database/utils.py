d0937a7cd96f4637991ffff064d9d350b75615bf
gramatan <78417563+gramatan@users.noreply.github.com>
2023-08-04 18:27:22 +0600

    hooks

--- app/database/utils.py
+++ app/database/utils.py
@@ -2,7 +2,7 @@ import logging
 
 from sqlalchemy.ext.asyncio import create_async_engine
 
-from app.database.base import async_session, Base
+from app.database.base import Base, async_session
 
 
 async def get_db():
@@ -11,18 +11,18 @@ async def get_db():
 
 
 async def create_tables():
-    logging.info("Creating tables")
-    SQLALCHEMY_DATABASE_URL = "postgresql+asyncpg://ylab:no_secure_password@localhost/resto"
+    logging.info('Creating tables')
+    SQLALCHEMY_DATABASE_URL = 'postgresql+asyncpg://ylab:no_secure_password@localhost/resto'
     engine = create_async_engine(SQLALCHEMY_DATABASE_URL)
     async with engine.begin() as conn:
         await conn.run_sync(Base.metadata.create_all)
-    logging.info("Tables created")
+    logging.info('Tables created')
 
 
 async def drop_tables():
-    logging.info("Dropping tables")
-    SQLALCHEMY_DATABASE_URL = "postgresql+asyncpg://ylab:no_secure_password@localhost/resto"
+    logging.info('Dropping tables')
+    SQLALCHEMY_DATABASE_URL = 'postgresql+asyncpg://ylab:no_secure_password@localhost/resto'
     engine = create_async_engine(SQLALCHEMY_DATABASE_URL)
     async with engine.begin() as conn:
         await conn.run_sync(Base.metadata.drop_all)
-    logging.info("Tables dropped")
+    logging.info('Tables dropped')
