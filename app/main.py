134fc136970b9b67fbed45e0f15c60f5bdb30634
gramatan <78417563+gramatan@users.noreply.github.com>
2023-08-04 21:18:00 +0600

    Add redis repo. include redis repo for submenus. add redis-commander for test purposes

--- app/main.py
+++ app/main.py
@@ -1,5 +1,6 @@
 import logging
 
+import redis  # type: ignore
 from fastapi import FastAPI
 
 from app.routers import dish_router, menu_router, submenu_router
@@ -10,3 +11,5 @@ app = FastAPI()
 app.include_router(menu_router.router, prefix='/api/v1', tags=['menus'])
 app.include_router(submenu_router.router, prefix='/api/v1/menus/{menu_id}', tags=['submenus'])
 app.include_router(dish_router.router, prefix='/api/v1/menus/{menu_id}/submenus/{submenu_id}', tags=['dishes'])
+
+redis_client = redis.Redis(host='redis', port=6379, db=0)
