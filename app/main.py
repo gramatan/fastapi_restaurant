921575a632b864b43ac58d69f9007bd1b64cf63d
gramatan <78417563+gramatan@users.noreply.github.com>
2023-08-04 12:58:39 +0600

    Move submenu handlers to separate file + fix typings

--- app/main.py
+++ app/main.py
@@ -22,32 +22,7 @@ logging.basicConfig(level=logging.INFO)
 app = FastAPI()
 
 app.include_router(menu_router.router, prefix="/api/v1/menus", tags=["menus"])
-
-
-# All about our Submenus.
-@app.get("/api/v1/menus/{menu_id}/submenus")
-async def get_submenus(menu_id: int, db: Session = Depends(get_db)) -> List[SubMenuResponse]:
-    return await read_submenus(db, menu_id)
-
-
-@app.post("/api/v1/menus/{menu_id}/submenus", status_code=201)
-async def post_submenu(submenu: SubMenuBase, menu_id: int, db: Session = Depends(get_db)) -> SubMenuResponse:
-    return await create_submenu(db, submenu, menu_id)
-
-
-@app.get("/api/v1/menus/{menu_id}/submenus/{submenu_id}")
-async def get_submenu(menu_id: int, submenu_id: int, db: Session = Depends(get_db)) -> SubMenuResponse:
-    return await read_submenu(db, submenu_id, menu_id)
-
-
-@app.patch("/api/v1/menus/{menu_id}/submenus/{submenu_id}")
-async def patch_submenu(menu_id: int, submenu_id: int, submenu: SubMenuBase, db: Session = Depends(get_db)) -> SubMenuResponse:
-    return await update_submenu(db, submenu_id, submenu, menu_id)
-
-
-@app.delete("/api/v1/menus/{menu_id}/submenus/{submenu_id}")
-async def delete_submenu(menu_id: int, submenu_id: int, db: Session = Depends(get_db)) -> dict:
-    return await del_submenu(db, submenu_id, menu_id)
+app.include_router(menu_router.router, prefix="/api/v1/menus/{menu_id}/submenus", tags=["submenus"])
 
 
 # All about our Dishes:
