ebf95c1a4100b69ccca2b0f191c867bda4f23d7e
gramatan <78417563+gramatan@users.noreply.github.com>
2023-08-05 14:34:09 +0600

    Add redis clear cache

--- create_tables.py
+++ create_tables.py
@@ -2,13 +2,20 @@ import asyncio
 import logging
 
 from app.database.utils import create_tables, drop_tables
+from app.repository.redis_cache import RedisCache, get_redis_client
 
 logging.basicConfig(level=logging.INFO)
 
 
 async def main():
+    redis_client = get_redis_client()
+    redis_cache = RedisCache(redis_client)
+
     await drop_tables()
+    redis_cache.clear_cache('*')
+
     await create_tables()
+
 
 if __name__ == '__main__':
     asyncio.run(main())
