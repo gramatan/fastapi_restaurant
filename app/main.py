5c1eddca0c42864ef055a749369a88b71d04ac1c
Andrey <megagran.work@gmail.com>
2023-08-11 12:41:19 +0600

    Useless var delete

--- app/main.py
+++ app/main.py
@@ -1,6 +1,5 @@
 import logging
 
-import redis  # type: ignore
 from fastapi import FastAPI
 
 from app.routers import dish_router, menu_router, submenu_router
@@ -11,5 +10,3 @@ app = FastAPI()
 app.include_router(menu_router.router, prefix='/api/v1', tags=['menus'])
 app.include_router(submenu_router.router, prefix='/api/v1/menus/{menu_id}', tags=['submenus'])
 app.include_router(dish_router.router, prefix='/api/v1/menus/{menu_id}/submenus/{submenu_id}', tags=['dishes'])
-
-redis_client = redis.Redis(host='redis', port=6379, db=0)
