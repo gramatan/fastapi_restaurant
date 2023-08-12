da01abd34ad3124e410069ce519fe290d41974f1
Andrey <megagran.work@gmail.com>
2023-08-13 00:08:35 +0600

    Task work somehow. but still TBU

--- admin_task.py
+++ admin_task.py
@@ -1,22 +1,18 @@
 import asyncio
+import json
+import os
 from datetime import timedelta
 
 import openpyxl
 from celery import Celery
 
 from app.repository.admin import (
-    create_dish,
-    create_menu,
-    create_submenu,
-    del_dish,
-    del_menu,
-    del_submenu,
-    dish_exists,
-    menu_exists,
-    submenu_exists,
-    update_dish,
-    update_menu,
-    update_submenu,
+    check_and_del_dish,
+    check_and_del_menu,
+    check_and_del_submenu,
+    create_update_dish,
+    create_update_menu,
+    create_update_submenu,
 )
 
 MenuType = list[tuple[int, str, str]]
@@ -24,11 +20,29 @@ SubMenuType = list[tuple[int, int, str, str]]
 DishType = list[tuple[int, int, int, str, str, str]]
 GLOBAL_DATA: dict[str, list]
 
-GLOBAL_DATA = {
-    'menus': [],
-    'submenus': [],
-    'dishes': []
-}
+global_data_path = 'global_data.json'
+
+
+def load_global_data():
+    if os.path.exists(global_data_path):
+        try:
+            with open(global_data_path, encoding='utf-8') as f:
+                return json.load(f)
+        except Exception as e:
+            print(f'Error loading global data: {e}')
+            return {'menus': [], 'submenus': [], 'dishes': []}
+    return {'menus': [], 'submenus': [], 'dishes': []}
+
+
+def save_global_data(data: dict[str, list]):
+    try:
+        with open(global_data_path, 'w', encoding='utf-8') as f:
+            json.dump(data, f, indent=4, ensure_ascii=False)
+    except Exception as e:
+        print(f'Error saving global data: {e}')
+
+
+GLOBAL_DATA = load_global_data()
 
 
 async def read_excel_to_data(filename: str) -> dict[str, list]:
@@ -48,83 +62,72 @@ async def read_excel_to_data(filename: str) -> dict[str, list]:
     for row in sheet.iter_rows(values_only=True):
         if row[0] is not None:  # Это меню
             last_menu_id = row[0]
-            menus.append((row[0], row[1], row[2]))
+            menus.append([row[0], row[1], row[2]])
         elif row[1] is not None:  # Это подменю
             last_submenu_id = row[1]
-            submenus.append((last_menu_id, row[1], row[2], row[3]))
+            submenus.append([last_menu_id, row[1], row[2], row[3]])
         else:  # Это блюдо
-            dishes.append((last_menu_id, last_submenu_id, row[2], row[3], row[4], row[5]))
+            dishes.append([last_menu_id, last_submenu_id, row[2], row[3], row[4], row[5]])
 
     return {'menus': menus, 'submenus': submenus, 'dishes': dishes}
 
 
-async def compare_data(new_data: dict[str, list]) -> dict[str, tuple[list, list]]:
-    """
-    Сравнивает новые данные с глобальной переменной и составляет списки для CRUD-операций.
-    """
-    menus_to_create_or_update = [menu for menu in new_data['menus'] if (menu not in GLOBAL_DATA['menus']) or (
-        menu in GLOBAL_DATA['menus'] and menu != GLOBAL_DATA['menus'][GLOBAL_DATA['menus'].index(menu)])]
+def get_manual_id_from_data(item, data_type):
+    if data_type == 'menus':
+        return item[0]
+    elif data_type == 'submenus':
+        return f'{item[0]}:{item[1]}'
+    elif data_type == 'dishes':
+        return f'{item[0]}:{item[1]}:{item[2]}'
+    else:
+        return None
 
-    menus_to_delete = [menu for menu in GLOBAL_DATA['menus'] if menu not in new_data['menus']]
 
-    submenus_to_create_or_update = [submenu for submenu in new_data['submenus'] if (submenu not in GLOBAL_DATA['submenus']) or (
-        submenu in GLOBAL_DATA['submenus'] and submenu != GLOBAL_DATA['submenus'][GLOBAL_DATA['submenus'].index(submenu)])]
-    submenus_to_delete = [submenu for submenu in GLOBAL_DATA['submenus'] if submenu not in new_data['submenus']]
+async def compare_data(new_data: dict[str, list]) -> dict[str, list]:
+    result = {}
 
-    dishes_to_create_or_update = [dish for dish in new_data['dishes'] if (dish not in GLOBAL_DATA['dishes']) or (
-        dish in GLOBAL_DATA['dishes'] and dish != GLOBAL_DATA['dishes'][GLOBAL_DATA['dishes'].index(dish)])]
-    dishes_to_delete = [dish for dish in GLOBAL_DATA['dishes'] if dish not in new_data['dishes']]
+    for data_type in ['menus', 'submenus', 'dishes']:
+        old_manual_ids = {get_manual_id_from_data(item, data_type): item for item in GLOBAL_DATA[data_type]}
+        new_manual_ids = {get_manual_id_from_data(item, data_type): item for item in new_data[data_type]}
 
-    result = {
-        'menus': (menus_to_create_or_update, menus_to_delete),
-        'submenus': (submenus_to_create_or_update, submenus_to_delete),
-        'dishes': (dishes_to_create_or_update, dishes_to_delete)
-    }
+        to_delete = [item for manual_id, item in old_manual_ids.items() if manual_id not in new_manual_ids]
+        to_create = [item for manual_id, item in new_manual_ids.items() if manual_id not in old_manual_ids]
+
+        to_update = [new_item for manual_id, new_item in new_manual_ids.items() if
+                     manual_id in old_manual_ids and new_item != old_manual_ids[manual_id]]
+
+        result[data_type] = [to_create + to_update, to_delete]
+
     return result
 
 
-async def process_crud(compared_data: dict[str, tuple[list, list]]):
+async def process_crud(compared_data: dict[str, list]):
+    # Menus
     for menu in compared_data['menus'][0]:
         manual_id = f'{menu[0]}'
-        exists = await menu_exists(manual_id)
-        if exists:
-            await update_menu(manual_id, menu)
-        else:
-            await create_menu(menu, manual_id)
+        await create_update_menu(menu, manual_id)
 
     for menu in compared_data['menus'][1]:
         manual_id = f'{menu[0]}'
-        exists = await menu_exists(manual_id)
-        if exists:
-            await del_menu(manual_id)
+        await check_and_del_menu(manual_id)
 
+    # Submenus
     for submenu in compared_data['submenus'][0]:
         manual_id = f'{submenu[0]}:{submenu[1]}'
-        exists = await submenu_exists(manual_id)
-        if exists:
-            await update_submenu(manual_id, submenu)
-        else:
-            await create_submenu(submenu, manual_id)
+        await create_update_submenu(submenu, manual_id)
 
     for submenu in compared_data['submenus'][1]:
         manual_id = f'{submenu[0]}:{submenu[1]}'
-        exists = await submenu_exists(manual_id)
-        if exists:
-            await del_submenu(manual_id)
+        await check_and_del_submenu(manual_id)
 
+    # Dishes
     for dish in compared_data['dishes'][0]:
         manual_id = f'{dish[0]}:{dish[1]}:{dish[2]}'
-        exists = await dish_exists(manual_id)
-        if exists:
-            await update_dish(manual_id, dish)
-        else:
-            await create_dish(dish, manual_id)
+        await create_update_dish(dish, manual_id)
 
     for dish in compared_data['dishes'][1]:
         manual_id = f'{dish[0]}:{dish[1]}:{dish[2]}'
-        exists = await dish_exists(manual_id)
-        if exists:
-            await del_dish(manual_id)
+        await check_and_del_dish(manual_id)
 
 
 celery_app = Celery('admin_task')
@@ -147,10 +150,24 @@ celery_app.conf.update(
 
 
 async def main_async():
+    """
+    Основная функция, которая выполняется в Celery.
+    """
     new_data = await read_excel_to_data('admin/Menu.xlsx')
+    print(f'new_data = {new_data}')
     compared = await compare_data(new_data)
     await process_crud(compared)
-    GLOBAL_DATA.update(new_data)
+
+    to_save = {
+        'menus': new_data['menus'],
+        'submenus': new_data['submenus'],
+        'dishes': new_data['dishes']
+    }
+
+    # debug purposes
+    save_global_data(to_save)
+    GLOBAL_DATA_test = load_global_data()
+    print(f'global = {GLOBAL_DATA_test}')
 
 
 @celery_app.task
--- app/repository/admin.py
+++ app/repository/admin.py
@@ -2,119 +2,94 @@ from sqlalchemy import select
 
 from app.database.base import Dish, Menu, SubMenu, async_session
 
-
-async def menu_exists(manual_id: str) -> bool:
-    async with async_session() as session:
-        result = await session.execute(select(Menu.id).filter(Menu.manual_id == manual_id))
-        return result.scalar_one_or_none() is not None
-
-
-async def submenu_exists(manual_id: str) -> bool:
-    async with async_session() as session:
-        result = await session.execute(select(SubMenu.id).filter(SubMenu.manual_id == manual_id))
-        return result.scalar_one_or_none() is not None
-
-
-async def dish_exists(manual_id: str) -> bool:
-    async with async_session() as session:
-        result = await session.execute(select(Dish.id).filter(Dish.manual_id == manual_id))
-        return result.scalar_one_or_none() is not None
-
-
 MenuType = list[tuple[int, str, str]]
 SubMenuType = list[tuple[int, int, str, str]]
 DishType = list[tuple[int, int, int, str, str, str]]
 
 
-async def create_menu(menu: MenuType, manual_id: str):
-    async with async_session() as session:
-        db_menu = Menu(title=menu[1], description=menu[2], manual_id=f'{manual_id}')
-        session.add(db_menu)
-        await session.commit()
-        await session.refresh(db_menu)
-        return db_menu.id
-
-
-async def create_submenu(submenu: SubMenuType, manual_id: str):
-    async with async_session() as session:
-        db_submenu = SubMenu(title=submenu[2], description=submenu[3], menu_id=submenu[1], manual_id=manual_id)
-        session.add(db_submenu)
-        await session.commit()
-        await session.refresh(db_submenu)
-        return db_submenu.id
-
-
-async def create_dish(dish: DishType, manual_id: str):
-    async with async_session() as session:
-        db_dish = Dish(
-            title=dish[3],
-            description=dish[4],
-            price=dish[5],
-            submenu_id=dish[1],
-            manual_id=manual_id
-        )
-        session.add(db_dish)
-        await session.commit()
-        await session.refresh(db_dish)
-        return db_dish.id
-
-
-async def update_menu(manual_id: str, menu: MenuType):
+async def create_update_menu(menu: MenuType, manual_id: str):
     async with async_session() as session:
         result = await session.execute(select(Menu).filter(Menu.manual_id == manual_id))
         db_menu = result.scalar_one_or_none()
+
         if db_menu:
             db_menu.title = menu[1]
             db_menu.description = menu[2]
             await session.commit()
-            return db_menu.manual_id
+        else:
+            db_menu = Menu(title=menu[1], description=menu[2], manual_id=f'{manual_id}')
+            session.add(db_menu)
+            await session.commit()
+            await session.refresh(db_menu)
+        return manual_id, menu[1], menu[2]
 
 
-async def update_submenu(manual_id: str, submenu: SubMenuType):
+async def create_update_submenu(submenu: SubMenuType, manual_id: str):
     async with async_session() as session:
         result = await session.execute(select(SubMenu).filter(SubMenu.manual_id == manual_id))
         db_submenu = result.scalar_one_or_none()
+
+        menu_manual_id = manual_id.rsplit(':', 1)[0]
+        menu_id_result = await session.execute(select(Menu.id).filter(Menu.manual_id == menu_manual_id))
+        menu_id = menu_id_result.scalar_one_or_none()
+
         if db_submenu:
             db_submenu.title = submenu[2]
             db_submenu.description = submenu[3]
             await session.commit()
-            return db_submenu.manual_id
+        else:
+            new_submenu = SubMenu(title=submenu[2], description=submenu[3], menu_id=menu_id, manual_id=manual_id)
+            session.add(new_submenu)
+            await session.commit()
+            await session.refresh(new_submenu)
+        return manual_id, submenu[2], submenu[3]
 
 
-async def update_dish(manual_id: str, dish: DishType):
+async def create_update_dish(dish: DishType, manual_id: str):
     async with async_session() as session:
         result = await session.execute(select(Dish).filter(Dish.manual_id == manual_id))
         db_dish = result.scalar_one_or_none()
+
+        submenu_manual_id = manual_id.rsplit(':', 1)[0]
+        submenu_id_result = await session.execute(select(SubMenu.id).filter(SubMenu.manual_id == submenu_manual_id))
+        submenu_id = submenu_id_result.scalar_one_or_none()
+
         if db_dish:
             db_dish.title = dish[3]
             db_dish.description = dish[4]
             db_dish.price = dish[5]
             await session.commit()
-            return db_dish.manual_id
+        else:
+            new_dish = Dish(title=dish[3], description=dish[4], price=dish[5],
+                            submenu_id=submenu_id, manual_id=manual_id)
+            session.add(new_dish)
+            await session.commit()
+            await session.refresh(new_dish)
+        return manual_id, dish[3], dish[4], dish[5]
 
 
-async def del_menu(manual_id: str):
+async def check_and_del_menu(manual_id: str):
     async with async_session() as session:
         result = await session.execute(select(Menu).filter(Menu.manual_id == manual_id))
         db_menu = result.scalar_one_or_none()
         if db_menu:
-            session.delete(db_menu)
+            await session.delete(db_menu)
             await session.commit()
 
 
-async def del_submenu(manual_id: str):
+async def check_and_del_submenu(manual_id: str):
     async with async_session() as session:
         result = await session.execute(select(SubMenu).filter(SubMenu.manual_id == manual_id))
         db_submenu = result.scalar_one_or_none()
         if db_submenu:
-            session.delete(db_submenu)
+            await session.delete(db_submenu)
             await session.commit()
 
 
-async def del_dish(manual_id: str):
+async def check_and_del_dish(manual_id: str):
     async with async_session() as session:
         result = await session.execute(select(Dish).filter(Dish.manual_id == manual_id))
         db_dish = result.scalar_one_or_none()
         if db_dish:
-            session.delete(db_dish)
+            await session.delete(db_dish)
             await session.commit()
